71e5e2cfc23858a227c295d1acaa8a8a5c2fc4fe
dilshan27 <damithadilshan27@gmail.com>
2022-05-20 17:25:05 +0530

    add dashboard

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane context;
+
+    public void signInOnAction(ActionEvent actionEvent) throws IOException {
+        util.Utilities.loadui(context,"dashboard-form");
+    }
 }
