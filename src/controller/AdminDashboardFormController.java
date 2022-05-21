94dcf159b1967bdefb5734c23a6db3e8a8cca9bc
dilshan27 <damithadilshan27@gmail.com>
2022-05-21 14:40:00 +0530

    update

--- src/controller/AdminDashboardFormController.java
+++ src/controller/AdminDashboardFormController.java
@@ -1,11 +1,41 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class AdminDashboardFormController {
-    public JFXButton btnItem;
+    public AnchorPane context;
+    public JFXButton btnAddNewItem;
+    public JFXTextField txtCode;
+    public JFXTextField txtDescription;
+    public JFXTextField txtUnitPrice;
+    public JFXTextField txtQtyOnHand;
+    public JFXButton btnSave;
+    public JFXButton btnDelete;
+    public TableView tblItems;
+    public JFXTextField txtPackageSize;
 
-    public void AdministratorLogin(){
-        btnItem.setVisible(true);
+
+    public void logoutOnAction(ActionEvent actionEvent) throws IOException {
+        context.getChildren().clear();
+        Parent parent= FXMLLoader.load(getClass().getResource("../view/login-form.fxml"));
+        context.getChildren().add(parent);
+    }
+
+
+    public void btnAddNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
     }
 }
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,18 +1,13 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import view.util.Utilities;
+
 
 import java.io.IOException;
 
@@ -22,47 +17,12 @@ public class LoginFormController {
     public JFXPasswordField txtPwp;
     public JFXTextField txtUser;
     public JFXButton btnAdministratorSign;
-    public Label btnCreate;
-    public JFXComboBox cmbStatus;
-    public JFXButton btnSignup;
+
+
     public JFXButton btnCashierSign;
 
 
     public void initialize() {
-        ObservableList observableList = FXCollections.observableArrayList();
-        observableList.add("Administrator");
-        observableList.add("Cashier");
-        cmbStatus.setItems(observableList);
-    }
-
-    public void createAccountOnAction(MouseEvent mouseEvent) throws IOException {
-        createVisible();
-    }
-
-    public void signupOnAction(ActionEvent actionEvent) {
-        if (txtUser.getText().isEmpty() && txtPwp.getText().isEmpty() && cmbStatus.getValue().equals("")) {
-            System.out.println("create");
-        } else {
-
-            loginVisible();
-        }
-
-    }
-
-    public void createVisible() {
-        btnAdministratorSign.setVisible(false);
-        btnCashierSign.setVisible(false);
-        btnCreate.setVisible(false);
-        cmbStatus.setVisible(true);
-        btnSignup.setVisible(true);
-    }
-
-    public void loginVisible() {
-        btnAdministratorSign.setVisible(true);
-        btnCashierSign.setVisible(true);
-        btnCreate.setVisible(true);
-        cmbStatus.setVisible(false);
-        btnSignup.setVisible(false);
     }
 
     public void AdministratorSignInOnAction(ActionEvent actionEvent) throws IOException {
