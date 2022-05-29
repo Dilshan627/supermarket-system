b5558a859ff1970f9ee43a7409e93ca6bb3c5aa7
dilshan27 <damithadilshan27@gmail.com>
2022-05-29 09:53:42 +0530

    barchart add

--- src/controller/AdministratorFormController.java
+++ src/controller/AdministratorFormController.java
@@ -2,12 +2,8 @@ package controller;
 
 import bo.BOFactory;
 import bo.custom.AdministratorBO;
-import javafx.beans.binding.Bindings;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.chart.BarChart;
-import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.AnchorPane;
 import model.CustomDTO;
@@ -20,58 +16,28 @@ public class AdministratorFormController {
     private final AdministratorBO bo = (AdministratorBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.Administrator);
     public AnchorPane sideContext;
     public AnchorPane context;
-    public PieChart picChart;
     public BarChart barChart;
 
     public void initialize() {
-        try {
-            ArrayList<CustomDTO> customDTOS = bo.MostSellItem();
-            for (int i = 0; i < customDTOS.size(); i++) {
-                System.out.print(customDTOS.get(i).getDescription());
-                System.out.println(customDTOS.get(i).getOrderqty());
-            }
+        orderBarChart();
+    }
 
+    private void orderBarChart() {
+        XYChart.Series dataSeries = new XYChart.Series();
+        dataSeries.setName("Most Sell Item");
+        ArrayList<CustomDTO> customDTOS = null;
+        try {
+            customDTOS = bo.MostSellItem();
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        orderPicChart();
-
-    }
-
-    private void orderPicChart() {
-//        ObservableList<PieChart.Data> productDate = FXCollections.observableArrayList();
-//        try {
-//            ArrayList<CustomDTO> customDTOS = bo.MostSellItem();
-//            for (int i = 0; i < customDTOS.size(); i++) {
-//                productDate.add(new PieChart.Data(customDTOS.get(i).getDescription(),customDTOS.get(i).getOrderqty()));
-//            }
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        productDate.forEach(data -> data.nameProperty().bind(Bindings.concat(data.getName()," ",data.pieValueProperty())));
-//        picChart.setData(productDate);
-//        picChart.setTitle("Most Sell Item");
-
-
-
-        XYChart.Series dataSeries = new XYChart.Series();
-        dataSeries.setName("2014");
-
-        dataSeries.getData().add(new XYChart.Data("Desktop", 178));
-        dataSeries.getData().add(new XYChart.Data("Phone"  , 65));
-        dataSeries.getData().add(new XYChart.Data("Tablet"  , 23));
-
+        for (int i = 0; i < customDTOS.size(); i++) {
+                dataSeries.getData().add(new XYChart.Data(customDTOS.get(i).getDescription(),customDTOS.get(i).getOrderqty()));
+            }
         barChart.getData().add(dataSeries);
-
-
     }
-
-
-
 
     public void dashboardOnAction(ActionEvent actionEvent) throws IOException {
         util.navigation.navigate(context, "administrator");
